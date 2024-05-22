51d2fceaa413c3359f32d6cb9fa40c1bc9eb8549
Rafael Matte <rafael.matte@meliuz.com.br>
2024-05-22 00:40:50 -0300

    fix: Background image

--- src/app/components/Background/styles.ts
+++ src/app/components/Background/styles.ts
@@ -1,17 +1,19 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  display: grid;
   height: 100vh;
   overflow: hidden;
-  place-items: center;
   position: absolute;
   width: 100vw;
 `;
 
 export const Image = styled.img`
-  filter: blur(40px);
-  height: 110%;
+  bottom: 75px;
+  filter: blur(70px);
+  height: calc(100% + 150px);
   max-width: none;
-  width: 110%;
+  object-fit: cover;
+  position: relative;
+  right: 75px;
+  width: calc(100% + 150px);
 `;
